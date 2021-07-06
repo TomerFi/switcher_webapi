0c3e472b6581c1ba981286f41785587d22e7e567
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-07-06 09:37:57 +0300

    test: added test cases for the set_auto_shutdown endpoint

--- app/tests/test_web_app.py
+++ app/tests/test_web_app.py
@@ -14,12 +14,13 @@
 
 """Test cases for the web application."""
 
+from datetime import timedelta
 from unittest.mock import AsyncMock, Mock, patch
 
 from aiohttp import web
 from aioswitcher.api import Command
 from assertpy import assert_that
-from pytest import fixture
+from pytest import fixture, mark
 
 from .. import webapp
 
@@ -102,47 +103,29 @@ async def test_erroneous_get_state_get_request(
     assert_that(await response.json()).contains_entry({"error": "blabla"})
 
 
+@mark.parametrize("query_params,expected_values", [
+    ("", (Command.ON, 0)),
+    # &minutes=15
+    ("&" + webapp.KEY_MINUTES + "=15", (Command.ON, 15)),
+])
 @patch("aioswitcher.api.SwitcherApi.control_device")
-async def test_successful_turn_on_without_time_limit_post_request(
+async def test_successful_turn_on_post_request(
     control_device,
     response_serializer,
     response_mock,
     api_connect,
     api_disconnect,
     api_client,
+    query_params,
+    expected_values,
 ):
     # stub control_device to return mock state response
     control_device.return_value = response_mock
-    # send post request for turn_on endpoint: /switcher/turn_on?id=ab1c2d&ip=1.2.3.4
-    response = await api_client.post(webapp.ENDPOINT_TURN_ON + fake_device_qparams)
+    # send post request for turn_on endpoint: /switcher/turn_on?id=ab1c2d&ip=1.2.3.4...
+    response = await api_client.post(webapp.ENDPOINT_TURN_ON + fake_device_qparams + query_params)
     # verify mocks calling
     api_connect.assert_called_once()  # connect is always called
-    control_device.assert_called_once_with(Command.ON, 0)
-    response_serializer.assert_called_once_with(response_mock)
-    api_disconnect.assert_called_once()  # diconnect is always called
-    # assert the expected response
-    assert_that(response.status).is_equal_to(200)
-    assert_that(await response.json()).contains_entry(fake_serialized_data)
-
-
-@patch("aioswitcher.api.SwitcherApi.control_device")
-async def test_successful_turn_on_with_time_limit_post_request(
-    control_device,
-    response_serializer,
-    response_mock,
-    api_connect,
-    api_disconnect,
-    api_client,
-):
-    # stub control_device to return mock state response
-    control_device.return_value = response_mock
-    # send post request for turn_on endpoint: /switcher/turn_on?id=ab1c2d&ip=1.2.3.4&minutes=15
-    response = await api_client.post(
-        webapp.ENDPOINT_TURN_ON + fake_device_qparams + "&" + webapp.KEY_MINUTES + "=15"
-    )
-    # verify mocks calling
-    api_connect.assert_called_once()  # connect is always called
-    control_device.assert_called_once_with(Command.ON, 15)
+    control_device.assert_called_once_with(expected_values[0], expected_values[1])
     response_serializer.assert_called_once_with(response_mock)
     api_disconnect.assert_called_once()  # diconnect is always called
     # assert the expected response
@@ -237,6 +220,53 @@ async def test_erroneous_set_name_patch_request(
     # verify mocks calling
     api_connect.assert_called_once()  # connect is always called
     set_device_name.assert_called_once_with("newFakedName")
+    response_serializer.assert_not_called()
+    api_disconnect.assert_called_once()  # diconnect is always called
+    # assert the expected response
+    assert_that(response.status).is_equal_to(500)
+    assert_that(await response.json()).contains_entry({"error": "blabla"})
+
+
+@mark.parametrize("query_params,expected_timedelta", [
+    # &hours=2
+    ("&" + webapp.KEY_HOURS + "=2", timedelta(hours=2)),
+    # &hours=2&minutes=30
+    ("&" + webapp.KEY_HOURS + "=2&" + webapp.KEY_MINUTES + "=30", timedelta(hours=2, minutes=30)),
+])
+@patch("aioswitcher.api.SwitcherApi.set_auto_shutdown")
+async def test_successful_set_auto_shutdown_patch_request(
+    set_auto_shutdown,
+    response_serializer,
+    response_mock,
+    api_connect,
+    api_disconnect,
+    api_client,
+    query_params,
+    expected_timedelta,
+):
+    # stub set_auto_shutdown to return mock state response
+    set_auto_shutdown.return_value = response_mock
+    # send patch request for set_auto_shutdown endpoint: /switcher/set_auto_shutdown?id=ab1c2d&ip=1.2.3.4...
+    response = await api_client.patch(webapp.ENDPOINT_SET_AUTO_SHUTDOWN + fake_device_qparams + query_params)
+    # verify mocks calling
+    api_connect.assert_called_once()  # connect is always called
+    set_auto_shutdown.assert_called_once_with(expected_timedelta)
+    response_serializer.assert_called_once_with(response_mock)
+    api_disconnect.assert_called_once()  # diconnect is always called
+    # assert the expected response
+    assert_that(response.status).is_equal_to(200)
+    assert_that(await response.json()).contains_entry(fake_serialized_data)
+
+
+@patch("aioswitcher.api.SwitcherApi.set_auto_shutdown", side_effect=Exception("blabla"))
+async def test_erroneous_set_auto_shutdown_patch_request(
+    set_auto_shutdown, response_serializer, api_connect, api_disconnect, api_client
+):
+    # send patch request for set_auto_shutdown endpoint: /switcher/set_auto_shutdown?id=ab1c2d&ip=1.2.3.4&hours=2
+    response = await api_client.patch(webapp.ENDPOINT_SET_AUTO_SHUTDOWN + fake_device_qparams + "&" + webapp.KEY_HOURS + "=2")
+    # verify mocks calling
+    api_connect.assert_called_once()  # connect is always called
+    set_auto_shutdown.assert_called_once_with(timedelta(hours=2))
     response_serializer.assert_not_called()
     api_disconnect.assert_called_once()  # diconnect is always called
     # assert the expected response
